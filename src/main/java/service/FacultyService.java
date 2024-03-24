ec216c595ab5716172f5742cddcb4f267eb6e1fd
denis <deniz246@mail.ru>
2024-03-24 23:21:58 +0300

    1

---
+++ src/main/java/service/FacultyService.java
@@ -0,0 +1,10 @@
+package service;
+
+import model.Faculty;
+import model.Student;
+
+import java.util.HashMap;
+
+public class FacultyService {
+    private final HashMap<Long, Faculty> books = new HashMap<>();
+}
---
+++ src/main/java/service/StudentService.java
@@ -0,0 +1,10 @@
+package service;
+
+import model.Faculty;
+import model.Student;
+
+import java.util.HashMap;
+
+public class StudentService {
+    private final HashMap<Long, Student> books = new HashMap<>();
+}

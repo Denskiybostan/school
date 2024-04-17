7c29d5a057b6265fe760dc372698702369e32cdd
denis <deniz246@mail.ru>
2024-04-17 20:52:21 +0300

    1

--- src/main/java/repository/FacultyRepository.java
+++ src/main/java/repository/FacultyRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Collection;
 
 public interface FacultyRepository  extends JpaRepository<Faculty, Long> {
-    Collection<Faculty> findAllByColorAndNameIgnoreCase(String color, String name);
     Collection<Faculty> findAllByColorOrNameIgnoreCase(String color, String name);
 }

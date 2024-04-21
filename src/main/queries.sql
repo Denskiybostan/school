356f1246c0e553d88710cf5807409cfb4b41b178
denis <deniz246@mail.ru>
2024-04-21 22:20:34 +0300

    1

--- src/main/queries.sql
+++ src/main/queries.sql
@@ -1,5 +1,8 @@
-select from Student;
+select * from Student;
 
-select from faculty;
-select *
-from faculty where id = 1;
+select * from faculty;
+select * from student where age > 15 or where id > 1;
+select * from faculty where id > 3;
+select * from student where name is not null;
+select * from faculty wher color is not 'white';
+select * from student where id > 1;

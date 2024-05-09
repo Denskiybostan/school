e44f929f4a85e67015360ee652175cabcfb2161a
denis <deniz246@mail.ru>
2024-05-09 09:36:12 +0300

    2

--- src/main/queries.sql
+++ src/main/queries.sql
@@ -1,7 +1,7 @@
 select surname from student;
 select * from student where age between 10 and 20;
 select * from student where name like '%O%';
-select * from student where name is not null;
+select name from student where name is not null;
 select * from student order by age;
 alter table student add constraint age_check CHECK (age>= 16);
 insert  into student(age, name, faculty_id) values (17, 'aaaaa', null );
---
+++ src/main/resources/lesson-three-migration/liquibase/students.sql
@@ -0,0 +1,11 @@
+-- liquibase formated sql
+
+-- changeset denis:1
+create table students(
+    name text,
+    surname text
+)
+create table faculty(
+    name text,
+    color text
+)

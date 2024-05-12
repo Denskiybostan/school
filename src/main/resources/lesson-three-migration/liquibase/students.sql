284aafbc4f23e48f59930e57d3a7d8b98ef26123
denis <deniz246@mail.ru>
2024-05-12 08:59:49 +0300

    2

--- src/main/resources/lesson-three-migration/liquibase/students.sql
+++ src/main/resources/lesson-three-migration/liquibase/students.sql
@@ -1,11 +1,11 @@
 -- liquibase formated sql
 
 -- changeset denis:1
-create index students(
-    name text,
+create unique index students
+    on table_name text,
     surname text
 )
-create index faculty(
-    name text,
+create unique index faculty
+    on table_name text,
     color text
 )

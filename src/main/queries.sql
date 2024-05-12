abd58b6952f87f17996275778721d4e7849e412b
denis <deniz246@mail.ru>
2024-05-12 14:35:18 +0300

    2

--- src/main/queries.sql
+++ src/main/queries.sql
@@ -3,6 +3,16 @@ select * from student where age between 10 and 20;
 select * from student where name like '%O%';
 select name from student where name is not null;
 select * from student order by age;
-alter table student add constraint age_check CHECK (age>= 16);
+create table student(
+    constraint  name_uniue unique (name)
+);
+create table faculty(
+    constraint faculty_name_color_uniq unique (name, color)
+);
 insert  into student(age, name, faculty_id) values (17, 'aaaaa', null );
-alter table student add constraint name_unique unique  (name);
+create table student(
+    constraint age_check check (age >=16)
+);
+create table student(
+    set int default age(20)
+);

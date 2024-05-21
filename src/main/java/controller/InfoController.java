7ca8c291d2d3646f7eec3bf35519ef3bdb859295
denis <deniz246@mail.ru>
2024-05-21 09:11:40 +0300

    3

--- src/main/java/controller/InfoController.java
+++ src/main/java/controller/InfoController.java
@@ -3,21 +3,20 @@ package controller;
 import com.google.common.base.Stopwatch;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/info")
 public class InfoController {
     private final static Logger logger = (Logger) LoggerFactory.getLogger(InfoController.class);
     @Value("${server.port}")
-    private  int port;
+    private int port;
+
     @GetMapping("/port")
     public int port() {
         logger.info("Starting port{}");
@@ -25,19 +24,18 @@ public class InfoController {
     }
 
     @GetMapping("/sum")
-    public  int sum() {
+    public void sum() {
         var start = System.currentTimeMillis();
-        int sum = IntStream.iterate(1, a -> a + 1).limit(1_000_000).sum();
-        var end = System.currentTimeMillis() - start;
-        logger.info("Elapsed time:{}" + end);
-        return sum;
-    }
-
-    @GetMapping("/sum2")
-    public int sum2() {
-        var st = Stopwatch.createStarted();
-        int sum = IntStream.iterate(1, a -> a + 1).limit(1_000_000).sum();
-        logger.info("Elapsed time:{}" + st);
-        return sum;
+        new Thread(() -> {
+            int sum = IntStream.iterate(1, a -> a + 1).limit(1_000_000).sum();
+            var end = System.currentTimeMillis() - start;
+            logger.info("Elapsed time:{}" + end);
+        }).start();
+        new Thread(() -> {
+            var st = Stopwatch.createStarted();
+            int sum = IntStream.iterate(1, a -> a + 1).limit(1_000_000).sum();
+            logger.info("Elapsed time:{}" + st);
+        }).start();
     }
 }
+

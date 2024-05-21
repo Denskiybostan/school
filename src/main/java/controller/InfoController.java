9ef11f9d800cb2963e28fc367bacde62c899b310
denis <deniz246@mail.ru>
2024-05-21 20:34:38 +0300

    3

--- src/main/java/controller/InfoController.java
+++ src/main/java/controller/InfoController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.StudentService;
 
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/info")
@@ -16,7 +19,6 @@ public class InfoController {
     private final static Logger logger = (Logger) LoggerFactory.getLogger(InfoController.class);
     @Value("${server.port}")
     private int port;
-
     @GetMapping("/port")
     public int port() {
         logger.info("Starting port{}");
@@ -26,16 +28,17 @@ public class InfoController {
     @GetMapping("/sum")
     public void sum() {
         var start = System.currentTimeMillis();
-        new Thread(() -> {
-            int sum = IntStream.iterate(1, a -> a + 1).limit(1_000_000).sum();
-            var end = System.currentTimeMillis() - start;
-            logger.info("Elapsed time:{}" + end);
-        }).start();
-        new Thread(() -> {
-            var st = Stopwatch.createStarted();
-            int sum = IntStream.iterate(1, a -> a + 1).limit(1_000_000).sum();
-            logger.info("Elapsed time:{}" + st);
-        }).start();
+        int sum = IntStream.iterate(1, a -> a + 1).parallel().limit(1_000_000).sum();
+        var end = System.currentTimeMillis() - start;
+        logger.info("Elapsed time:{}" + end);
     }
+        public int sum2() {
+            var st = Stopwatch.createStarted();
+            int sum2 = IntStream.iterate(1, a -> a + 1).parallel().limit(1_000_000).sum();
+            logger.info("Elapsed time:{}" + st);
+            return sum2;
+    }
+
+
 }
 
